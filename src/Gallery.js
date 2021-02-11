685ae7f7abcd6711f8303aa10b8706d985dad554
Stephanie <stephanie.walker@sada.com>
2021-02-11 13:24:02 -0800

    added icon

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import Image from "./Image";
 import "./ImageList.css"
+import logo from './images/usa.png';
 
 export default class Gallery extends Component {
   render() {
     return (
       <div>
-        <div className="h1">
-          MEME GENERATOR
-        </div>
+        <div className="h1">MEME GENERATOR</div>
+        <img className="icon" src={logo} alt="Logo" style={{position: 'absolute', top: 8, right: 15, width: '5%', height: 100}}/>
 
         <div>
           <button className="btn-gallery" onClick={() => this.props.handleRandom()}>Random Meme</button>
