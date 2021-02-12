17d292adb4ced482bf5b4cc34a110f3be502abc4
Stephanie <stephanie.walker@sada.com>
2021-02-12 10:17:43 -0800

    updated styling and images

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
 import Image from "./Image";
 import "./ImageList.css"
-import logo from './images/usa.jpg';
+import logo from './images/canada.png';
+
 
 export default class Gallery extends Component {
   render() {
     return (
       <div>
-        <div className="h1">MEME GENERATOR</div>
-        <img className="icon" src={logo} alt="Logo" style={{position: 'absolute', top: 10, right: 15, width: '8%', height: 80}}/>
+        <div className="h1">
+          MEME GENERATOR 
+           </div>
+           <img className="icon" src={logo} alt="Logo" style={{position: 'absolute', top: 10, right: 15, width: '7%', height: 100}}/>
+        
+        
+       
 
         <div>
-          <button className="btn-gallery" onClick={() => this.props.handleRandom()}>Random Meme</button>
+          <button className="btn-gallery" onClick={() => this.props.handleRandom()}>Gimme a Random Meme</button>
+          
+        
         </div>
 
         <div className="image-list">
